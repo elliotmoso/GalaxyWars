afd70ecfed7537daff0ab8e9f9977d64bc136d42
root <root@saasbook>
2013-07-31 18:04:07 -0700

    First Commit add STARS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GalaxyWars::Application.config.secret_token = '32d96ce94abec1a084836833af2b8c15009827d86f56c33fcc61a514a9c0523b5f37586e433dd4b8696d6a75d005fd518d09506d780aee99b83fc53924348262'
